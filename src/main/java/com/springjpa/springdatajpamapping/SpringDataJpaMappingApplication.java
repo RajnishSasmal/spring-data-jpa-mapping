14572c542b266f2402a21e7e98a57e0740f1e156
Rajnish <rajnishsasmal@gmail.com>
2019-12-31 08:53:25 +0530

    new branch

--- src/main/java/com/springjpa/springdatajpamapping/SpringDataJpaMappingApplication.java
+++ src/main/java/com/springjpa/springdatajpamapping/SpringDataJpaMappingApplication.java
@@ -12,6 +12,7 @@ public class SpringDataJpaMappingApplication extends SpringBootServletInitialize
 		SpringApplication.run(SpringDataJpaMappingApplication.class, args);
 		System.out.println("Start the SPRING JPA!!!.........");
         System.out.println("After first PUSH YEAH!!!!!.........");
+		System.out.println("After first ne Branch!!!.........");
 
     }
 	@Override
